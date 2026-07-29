53fa7a9b5d0b9e749f504fdbef535b8cd93fef46
tdc <tdc@ubuntu.laptop>
2026-07-29 16:20:31 +0700

    start 13.DaemonSet

--- 03.deploy-stg-env.sh
+++
@@ -1,4 +0,0 @@
-# Tiến hành deploy Stg Env
-kubectl apply -f deploy-NS+Ingress.yaml
-kubectl apply -f deploy-main-service-in-ns.yaml --namespace stg-custom-ns
-
--- 04.delete-all.sh
+++
@@ -1,5 +0,0 @@
-# Tiến hành xoá toàn bộ resources trên 2 namspace + Ingress
-kubectl delete all --all --namespace dev-custom-ns
-kubectl delete all --all --namespace stg-custom-ns
-
-kubectl delete -f deploy-NS+Ingress.yaml
